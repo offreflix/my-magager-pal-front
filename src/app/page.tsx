87ff39a5acb1002c5002cc4e378e7133781b8255
João Victor <offreflix@gmail.com>
2024-08-31 01:16:02 -0300

    fix: remove unused fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,8 @@
 import { ModeToggle } from '@/components/ui/mode-toggle'
-import axios from 'axios'
 
 import Image from 'next/image'
 
 export default function Home() {
-  async function fetchUser() {
-    const teste = await axios.post('http://localhost:3000/auth/login')
-    console.log(teste)
-  }
-
-  fetchUser()
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
